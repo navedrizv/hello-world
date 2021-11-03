8ea73dd43b8aa393516c785a8eef923cfaf13603
TeckLabs.In <tecklabs.in@gmail.com>
2021-11-03 21:32:43 +0530

    Rust Hello

---
+++ hello-world/target/debug/deps/hello_world-ade27ca5303c91f3.d
@@ -0,0 +1,5 @@
+/Users/sayednaweedrizvi/Documents/GitHub/tecklabs/hello-world/hello-world/target/debug/deps/hello_world-ade27ca5303c91f3: src/main.rs
+
+/Users/sayednaweedrizvi/Documents/GitHub/tecklabs/hello-world/hello-world/target/debug/deps/hello_world-ade27ca5303c91f3.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/sayednaweedrizvi/Documents/GitHub/tecklabs/hello-world/hello-world/target/debug/hello-world: /Users/sayednaweedrizvi/Documents/GitHub/tecklabs/hello-world/hello-world/src/main.rs
